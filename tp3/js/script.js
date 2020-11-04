eafa5d4a4b447907e7bbf19da42da453949b99df
Matias Villegas <mati.villegas23@gmail.com>
2020-11-03 22:21:19 -0300

    terminado

--- tp3/js/script.js
+++ tp3/js/script.js
@@ -100,4 +100,17 @@ $(document).ready(function() {
             document.querySelector("#formEnviar").innerHTML = "Enviado!";
         }, 500)
     });
+
+    document.addEventListener("mousemove", parallax);
+
+    function parallax(e) {
+        this.querySelectorAll(".layer").forEach(layer => {
+            const speed = layer.getAttribute("data-speed")
+
+            const x = (window.innerWidth - e.pageX * speed) / 100;
+            const y = (window.innerHeight - e.pageY * speed) / 100;
+
+            layer.style.transform = 'translateX(' + x + 'px) ' + ' translateY(' + y + 'px)';
+        })
+    }
 });
